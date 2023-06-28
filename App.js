8380a2f1901599a5ea2612e5a17e9e65a202d23f
Minato235 <ankamasish@gmail.com>
2023-06-28 06:46:32 +0530

    started!

--- App.js
+++ App.js
@@ -1,15 +1,3 @@
 // const h1=React.createElement("h1",{id:"h1",xyz:"abx"},"Hello From React");
 import React from "react";
 import ReactDom from "react-dom";
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    [React.createElement("h1", {}, "I'am H1 Tag 1"),React.createElement("h1", {}, "I'am H1 Tag 2")]
-  )
-);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
