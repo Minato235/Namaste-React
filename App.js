a6cf2bbfe3311e8ab6c390b7bc91d135106c644d
Minato235 <ankamasish@gmail.com>
2023-07-01 06:50:03 +0530

    App starts

--- App.js
+++ App.js
@@ -1,13 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-//jsx
-const jsxHeading = <h1 className="head">Namaste React using Jsx</h1>;
-
-//React Componenet
-//Class based Componenet-Old
-//Function Componenet-New(normal js function)
 
 
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
