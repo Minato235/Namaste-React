c071edb0e90b61a2c50915bc7aa587c882216d12
Minato235 <ankamasish@gmail.com>
2023-07-10 03:46:40 +0530

    need to go back and reviw

--- App.js
+++ App.js
@@ -1538,6 +1538,8 @@ const Body = () => {
     <div className="body">
       <div className="Search">Search</div>
       <div className="res-container">
+         {/* <RestroCard resData={resList[0]}></RestroCard>
+        <RestroCard resData={resList[1]}></RestroCard> */}
         {resList.map((restaurant) => (
           <RestroCard resData={restaurant} />
         ))}
