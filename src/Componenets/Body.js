0fbf61a81fd535c1c504b44d22a3e67f96de40d7
Minato235 <ankamasish@gmail.com>
2023-07-24 07:56:09 +0530

    just try going back

--- src/Componenets/Body.js
+++ src/Componenets/Body.js
@@ -1,9 +1,29 @@
 import RestroCard from "./RestroCard";
-import { useState } from "react";
-import { resList } from "../util/data";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+
+// import { resList } from "../util/data";
 
 const Body = () => {
-  const [listOfRestro, setListOfRestro] = useState(resList);
+  const [listOfRestro, setListOfRestro] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.3569462&lng=78.6682395&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    // console.log(json);
+
+    setListOfRestro(json?.data?.cards[0]?.data?.data?.cards);
+  };
+  if(listOfRestro.length===0){
+    return <h1><Shimmer/></h1>
+  }
   return (
     <div className="body">
       <div className="Search">Search</div>
--- src/Componenets/RestroCard.js
+++ src/Componenets/RestroCard.js
@@ -4,16 +4,20 @@ const RestroCard = (props) => {
     backgroundColor: "#0f0f0",
   };
   const { resData } = props;
-  const { name, cuisines, avgRating, costForTwo, deliveryTime, id } =
-    resData?.data;
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    avgRating,
+    costForTwo,
+    deliveryTime,
+    id
+  } = resData?.data;
   return (
     <div className="res-card" style={styleCard}>
-      <img
-        className="res-logo"
-        src={urlsId + resData.data.cloudinaryImageId}
-      ></img>
+      <img className="res-logo" src={urlsId + cloudinaryImageId}></img>
       <h3>{name}</h3>
-      <h4>{cuisines.join(", ")}</h4>
+      <h4>{cuisines.join(",")}</h4>
       <h4>{deliveryTime} min delivery Time </h4>
       <h4>{avgRating} stars</h4>
     </div>
